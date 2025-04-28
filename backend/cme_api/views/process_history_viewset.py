a5df05dab078ecee28e2da22f551deea282f40fe
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-04-27 23:29:25 -0400

    ambiente frontend criado

--- backend/cme_api/views/process_history_viewset.py
+++ backend/cme_api/views/process_history_viewset.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from cme_api.models import ProcessHistory
 from cme_api.serializers import ProcessHistorySerializer
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 class ProcessHistoryViewSet(viewsets.ModelViewSet):
     queryset = ProcessHistory.objects.all()
@@ -9,3 +12,22 @@ class ProcessHistoryViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields = ['produto_serial', 'etapa', 'user']
+    
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name="serial", description="Código serial do produto", required=True, type=str),
+        ]
+    )
+    @action(detail=False, methods=['get'], url_path='traceability')
+    def traceability(self, request):
+        serial = request.query_params.get('serial')
+
+        if not serial:
+            return Response(
+                {"detail": "É necessário informar o parâmetro 'serial'."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        queryset = self.queryset.filter(serial__codigo_serial=serial).order_by('entry_data')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
